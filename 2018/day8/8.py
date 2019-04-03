a8c950a46c0c76ff10501e143c3bfdfb50a5f132
Robotyk <jakub.gizowski@gmail.com>
2019-04-04 00:09:55 +0200

    +add day8 solution

--- 2018/day8/8.py
+++ 2018/day8/8.py
@@ -39,3 +39,24 @@ def prepare_data(i):
 root = prepare_data(0)
 
 
+def sum_metadata(node):
+    sub_sum = 0
+    for x in node.children:
+        sub_sum = sub_sum + sum_metadata(x)
+    return sum(node.metadata) + sub_sum
+
+
+print(sum_metadata(root))
+
+
+def fancy_sum(node):
+    super_sum = 0
+    if node.number_of_children == 0:
+        return sum(node.metadata) + super_sum
+    for x in node.metadata:
+        if 0 < x <= len(node.children):
+            super_sum = super_sum + fancy_sum(node.children[x - 1])
+    return super_sum
+
+
+print(fancy_sum(root))
