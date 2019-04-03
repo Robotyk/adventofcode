31254ce4ac3d27335b6299f798833208001c567c
Robotyk <jakub.gizowski@gmail.com>
2019-04-03 02:25:39 +0200

    +add day8 data

--- 2018/day6/6.py
+++ 2018/day6/6.py
@@ -1,9 +1,12 @@
+file_name = "data"
+
+
 def get_boundaries():
     top = 999
     bot = 0
     left = 999
     right = 0
-    file_object = open("data", "r")
+    file_object = open(file_name, "r")
     for line in file_object:
         x_axis = int(line[:line.find(",")])
         y_axis = int(line[line.find(",") + 1:])
@@ -15,14 +18,14 @@ def get_boundaries():
             top = y_axis
         if y_axis > bot:
             bot = y_axis
-    return left, right, top, bot
+    return left - 1, right + 1, top - 1, bot + 1
 
 
 def init_dict():
     empty_dictionary = {}
     for i in range(boundaries[0], boundaries[1] + 1):
         for j in range(boundaries[2], boundaries[3] + 1):
-            empty_dictionary[str(i) + ":" + str(j)] = 999
+            empty_dictionary[str(i) + ":" + str(j)] = 9999
     return empty_dictionary
 
 
@@ -31,9 +34,9 @@ def calculate_dist(x_a, y_a, x_b, y_b):
 
 
 boundaries = get_boundaries()
-distances = init_dict()
-file_object = open("data", "r")
-distances2 = {}
+distance_to_closest_point = init_dict()
+file_object = open(file_name, "r")
+coords_of_closest_point = {}
 same_distances = {}
 for line in file_object:
     for i in range(boundaries[0], boundaries[1] + 1):
@@ -42,23 +45,23 @@ for line in file_object:
             y_from_line = int(line[line.find(",") + 1:])
             current_distance = calculate_dist(i, j, x_from_line, y_from_line)
             dictionary_key = str(i) + ":" + str(j)
-            if distances.get(dictionary_key) > current_distance:
-                distances[dictionary_key] = current_distance
-                distances2[dictionary_key] = (line[:line.find(",")]) + ":" + line[line.find(",") + 2:-1]
-            elif distances.get(dictionary_key) == current_distance:
+            if distance_to_closest_point.get(dictionary_key) > current_distance:
+                distance_to_closest_point[dictionary_key] = current_distance
+                coords_of_closest_point[dictionary_key] = (line[:line.find(",")]) + ":" + line[line.find(",") + 2:-1]
+            elif distance_to_closest_point.get(dictionary_key) == current_distance:
                 same_distances[dictionary_key] = current_distance
 
 counter_storage = {}
 black_list = set()
-for x in distances2:
-    if x in same_distances and distances2.get(x) == same_distances.get(x):
+for x in coords_of_closest_point:
+    if x in same_distances and coords_of_closest_point.get(x) == same_distances.get(x):
         continue
     if str(boundaries[0]) in x[:x.find(":")] or str(boundaries[1]) in x[:x.find(":")] or str(boundaries[2]) in x[x.find(":") + 1:] or str(boundaries[3]) in x[x.find(":") + 1:]:
-        black_list.add(distances2.get(x))
-    if distances2.get(x) not in counter_storage:
-        counter_storage[distances2.get(x)] = 1
+        black_list.add(coords_of_closest_point.get(x))
+    if coords_of_closest_point.get(x) not in counter_storage:
+        counter_storage[coords_of_closest_point.get(x)] = 1
     else:
-        counter_storage[distances2.get(x)] = counter_storage.get(distances2.get(x)) + 1
+        counter_storage[coords_of_closest_point.get(x)] = counter_storage.get(coords_of_closest_point.get(x)) + 1
 
 max = 0
 for x in counter_storage:
