9266691abd44dd8cfa682a61283230a4be320520
Robotyk <jakub.gizowski@gmail.com>
2019-04-16 22:43:12 +0200

    +add day14 part B solution

--- 2018/day14/14.py
+++ 2018/day14/14.py
@@ -1,4 +1,4 @@
-def create_recipe(size):
+def part_a_solution(size):
     recipes = ["3", "7"]
     i = 0
     j = 1
@@ -20,4 +20,40 @@ def create_recipe(size):
     return [recipes[a] for a in range(size, size + 10)]
 
 
-print(create_recipe(825401))
+def part_b_solution(input):
+    sequence = [i for i in input]
+
+    recipes = ["3", "7"]
+    i = 0
+    j = 1
+
+    while True:
+        first_current_recipe = int(recipes[i])
+        second_current_recipe = int(recipes[j])
+
+        sum = first_current_recipe + second_current_recipe
+        if sum < 10:
+            recipes.append(str(sum)[0])
+        else:
+            recipes.append(str(sum)[0])
+            recipes.append(str(sum)[1])
+
+        i = (first_current_recipe + 1 + i) % len(recipes)
+        j = (second_current_recipe + 1 + j) % len(recipes)
+
+        k = len(recipes) - len(sequence) - 1
+        if k >= 0 and matches(k, recipes, sequence):
+            return k
+
+
+def matches(k, recipes, sequence):
+    l = k
+    for i in sequence:
+        if recipes[l] != i:
+            return False
+        l += 1
+    return True
+
+
+print(part_a_solution(825401))
+print(part_b_solution("825401"))
